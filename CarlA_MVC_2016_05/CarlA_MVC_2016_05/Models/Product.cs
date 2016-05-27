e44e67f0a2c29d9105167dd96550e9f89a290460
C. Andersson <hejjacarl@hotmail.com>
2016-05-27 10:47:13 +0200

    inlämning

--- CarlA_MVC_2016_05/CarlA_MVC_2016_05/Models/Product.cs
+++ CarlA_MVC_2016_05/CarlA_MVC_2016_05/Models/Product.cs
@@ -10,19 +10,19 @@ namespace CarlA_MVC_2016_05.Models
 {
     public class Product
     {
-        [Required, Range(1, 10000000), ScaffoldColumn(false)]
+        [Required, ScaffoldColumn(false)]
         public int ProductId { get; set; }
         [Required]
         public string Name { get; set; }
         [Required, MaxLength(500)]
         public string ProductText { get; set; }
-        [Required, CustomAttribute.CustomAttributeChecker]
+        [Required, /*CustomAttribute.CustomAttributeChecker*/] //Freddie, när jag använder min CustomAttribute-klass smäller sidan. Jag begriper inte varför, då klassen ser OK ut?
         public int ProductNumber { get; set; }
-        [Required, Range(1, 9999999)]
-        public double Price { get; set; }
-        [Required, Range(0, 99999999)]
-        public int ProductsInStock { get; set; }
         [Required]
+        public double Price { get; set; }
+        [Required, Range(0, 999)]
+        public int ProductsInStock { get; set; }
+        [Required, Range(1, 2)]
         public int StoreId { get; set; }
 
     }
